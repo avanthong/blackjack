96061c8e95884ef7e0e6de0016a04a0ac2414382
AVT <adrien.vanthong@hds.com>
2017-03-04 15:13:52 -0500

    Removing extraneous comments

--- src/com/blackjack/Main.java
+++ src/com/blackjack/Main.java
@@ -17,14 +17,5 @@ public class Main {
         bj.playDealer();
         System.out.println(bj);
         System.out.println("Winners: " + bj.getWinners());
-
-        //TODO: remove this comment
-        /*
-        Idea for another game: Card version of "Elements".
-        Counters can remove value from the opponent's played cards.
-        Un-countered cards add up to the player's total points.
-        Special abilities for K/Q/J/A?
-        First person to reach 21 wins. (OR: more pts after 3 turns?)
-         */
     }
 }
